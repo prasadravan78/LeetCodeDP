16730eefda1623aa3871361055240d2494dcfd0d
Prasad Ravan <prasadcravan@gmail.com>
2022-08-03 01:11:25 +0530

    Day1

---
+++ LeetCodeDPDay1Program1/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace LeetCodeDPDay1Program1
+{
+    internal class FibonacciNumber
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Fib(32));
+            Console.ReadLine();
+        }
+
+        public static int Fib(int n)
+        {
+            if (n == 0)
+            {
+                return 0;
+            }
+            else if (n == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return Fib(n - 1) + Fib(n - 2);
+            }
+        }
+    }
+}
