e638f508e3f554b99fd00d8423ccad80fbb198b6
Prasad Ravan <prasadcravan@gmail.com>
2022-08-03 23:56:36 +0530

    Day2

---
+++ LeetCodeDPDay2Program1/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace LeetCodeDPDay2Program1
+{
+    internal class ClimbingStairs
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ClimbStairs(44));
+            Console.ReadLine();
+        }
+
+        public static int ClimbStairs(int n)
+        {
+            var twoSteps = 2;
+            var oneStep = 1;
+            var i = 3;
+            var requiredSteps = 0;
+
+            if (n == 1)
+            {
+                return oneStep;
+            }
+            else if (n == 2)
+            {
+                return twoSteps;
+            }
+
+            while (i <= n)
+            {
+                requiredSteps = twoSteps + oneStep;
+                oneStep = twoSteps;
+                twoSteps = requiredSteps;
+                i++;
+            }
+
+            return requiredSteps;
+        }
+    }
+}
---
+++ LeetCodeDPDay2Program2/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace LeetCodeDPDay2Program2
+{
+    internal class MinimumClimbingStairsCost
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MinCostClimbingStairs(new int[] { 1, 100, 1, 1, 1, 100, 1, 1, 100, 1 }));
+            Console.ReadLine();
+        }
+
+        public static int MinCostClimbingStairs(int[] cost)
+        {
+            if (cost.Length == 2)
+            {
+                return Math.Min(cost[0], cost[1]);
+            }
+
+            for (int i = cost.Length - 3; i >= 0; i--)
+            {
+                cost[i] += Math.Min(cost[i + 1], cost[i + 2]);
+            }
+
+            return Math.Min(cost[0], cost[1]);
+        }
+    }
+}
