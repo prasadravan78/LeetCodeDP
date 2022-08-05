b544273357fc171bb1435f04e0cc395134221bc9
Prasad Ravan <prasadcravan@gmail.com>
2022-08-05 23:22:58 +0530

    Day4

---
+++ LeetCodeDPDay4Program1/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace LeetCodeDPDay4Program1
+{
+    internal class JumpGame
+    {
+        static void Main(string[] args)
+        {
+            if (CanJump(new int[] { 3, 2, 1, 0, 4 }))
+            {
+                Console.WriteLine("Can Jump");
+            }
+
+            Console.ReadLine();
+        }
+
+        public static bool CanJump(int[] nums)
+        {
+            int nearest = nums.Length - 1;
+
+            for (int i = nums.Length - 2; i >= 0; i--)
+            {
+                if (i + nums[i] >= nearest)
+                {
+                    nearest = i;
+                }
+            }
+
+            return nearest == 0;
+        }
+    }
+}
---
+++ LeetCodeDPDay4Program2/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace LeetCodeDPDay4Program2
+{
+    internal class JumpGame2
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Jump(new int[] { 4, 3, 1, 1, 4,3 }));
+            Console.ReadLine();
+        }
+
+        public static int Jump(int[] nums)
+        {
+            int count = 0;  
+            int max = 0;
+            int current = 0;
+
+            for (int i = 0; i < nums.Length-1; i++)
+            { 
+                max = Math.Max(max, i+nums[i]);
+
+                if (i == current)
+                {
+                    count++;
+                    current = max;
+                }
+            }
+
+            return count;
+        }
+    }
+}
