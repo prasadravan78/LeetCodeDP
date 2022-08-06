9a1ef542c0eec93b339701ac58a691b69a990b1f
Prasad Ravan <prasadcravan@gmail.com>
2022-08-07 00:09:33 +0530

    Day5

---
+++ LeetCodeDPDay5Program1/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace LeetCodeDPDay5Program1
+{
+    internal class MaximumSumSubArray
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MaxSubArray(new int[] { 5, 4, -100, 7, 8 }));
+            Console.ReadLine();
+        }
+
+        public static int MaxSubArray(int[] nums)
+        {
+            int max = nums[0];
+            int currentMax = nums[0];
+
+            for (int i = 1; i < nums.Length; i++)
+            {
+                currentMax = Math.Max(currentMax + nums[i], nums[i]);
+                max = Math.Max(max , currentMax);
+            }
+
+            return max;
+        }
+    }
+}
---
+++ LeetCodeDPDay5Program2/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace LeetCodeDPDay5Program2
+{
+    internal class MaximumSumCircularArray
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MaxSubarraySumCircular(new int[] { 1, -2, 3, -2 }));
+            Console.ReadLine();
+        }
+
+        public static int MaxSubarraySumCircular(int[] nums)
+        {
+            var total = 0;
+            var max = nums[0];
+            var min = nums[0];
+            var currentMax = 0;
+            var currentMin = 0;
+
+            foreach (var num in nums)
+            {
+                total += num;
+
+                currentMax = Math.Max(num, currentMax + num);
+                currentMin = Math.Min(num, currentMin + num);
+
+                max = Math.Max(max, currentMax);
+                min = Math.Min(min, currentMin);
+            }
+
+            return max > 0 ? Math.Max(max, total - min) : max;
+        }
+    }
+}
