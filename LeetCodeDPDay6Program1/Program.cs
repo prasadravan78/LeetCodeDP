82e983fdc81059ef0c8603ffbd93834b60bacedc
Prasad Ravan <prasadcravan@gmail.com>
2022-08-07 23:46:59 +0530

    Day6

---
+++ LeetCodeDPDay6Program1/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace LeetCodeDPDay6Program1
+{
+    internal class MaximumProductInArray
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MaxProduct(new int[] { 0, 2 }));
+            Console.ReadLine();
+        }
+
+        public static int MaxProduct(int[] nums)
+        {
+            int max = nums[0];
+            int head = 1;
+            int tail = 1;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                head = (head == 0 ? 1 : head) * nums[i];
+                tail = (tail == 0 ? 1 : tail) * nums[nums.Length - 1 - i];
+
+                max = Math.Max(max, Math.Max(head, tail));
+            }
+
+            return Math.Max(max, Math.Max(head, tail));
+        }
+    }
+}
---
+++ LeetCodeDPDay6Program2/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace LeetCodeDPDay6Program2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetMaxLen(new int[] { 4, 5, -2, -11 }));
+            Console.ReadLine();
+        }
+
+        public static int GetMaxLen(int[] nums)
+        {
+            int positive = 0;
+            int negative = 0;
+            int max = 0;
+
+            foreach (var num in nums)
+            {
+                if (num == 0)
+                {
+                    positive = 0;
+                    negative = 0;
+                }
+                else if (num > 0)
+                {
+                    positive += 1;
+                    negative = negative == 0 ? 0 : negative + 1;
+                }
+                else
+                {
+                    var temp = positive;
+                    positive = negative == 0 ? 0 : negative + 1;
+                    negative = temp + 1;
+                }
+
+                max = Math.Max(max, positive);
+            }
+
+            return max;
+        }
+    }
+}
