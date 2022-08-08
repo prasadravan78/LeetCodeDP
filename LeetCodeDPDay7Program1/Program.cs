0b1f96a58344b2b3bd1498aa6714414729d1cb55
Prasad Ravan <prasadcravan@gmail.com>
2022-08-08 21:58:37 +0530

    Day7

---
+++ LeetCodeDPDay7Program1/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace LeetCodeDPDay7Program1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MaxScoreSightseeingPair(new int[] { 1,3,5 }));
+            Console.ReadLine();
+        }
+
+        public static int MaxScoreSightseeingPair(int[] values)
+        {
+            int max = 0;
+            int currentMax = 0;
+
+            for (int i = 0; i < values.Length; ++i)
+            {
+                max = Math.Max(max, currentMax + values[i] - i);
+                currentMax = Math.Max(currentMax, values[i] + i);
+            }
+
+            return max;
+        }
+    }
+}
---
+++ LeetCodeDPDay7Program3/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace LeetCodeDPDay7Program3
+{
+    internal class BuySellStock2
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MaxProfit(new int[] { 5, 4, 3, 2, 1 }));
+            Console.ReadLine();
+        }
+
+        public static int MaxProfit(int[] prices)
+        {
+            int buyPrice = prices[0];
+            int profit = 0;
+            int sellPrice = 0;
+
+            for (int i = 1; i < prices.Length; i++)
+            {
+                if (prices[i] > buyPrice)
+                {
+                    sellPrice = prices[i];
+                }
+                else if (buyPrice > prices[i])
+                {
+                    buyPrice = prices[i];
+                    sellPrice = 0;
+                }
+
+                if (sellPrice > 0 && sellPrice - buyPrice > 0)
+                {
+                    profit += sellPrice - buyPrice;
+                    sellPrice = 0;
+                    buyPrice = prices[i];
+                }
+            }
+
+            return profit;
+        }
+    }
+}
