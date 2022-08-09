7775727161f5334b39a1635d8929291cac26b1f4
Prasad Ravan <prasadcravan@gmail.com>
2022-08-09 22:51:03 +0530

    Day8

---
+++ LeetCodeDPDay8Program1/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace LeetCodeDPDay8Program1
+{
+    internal class BuySellStockWithCooldown
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MaxProfit(new int[] { 1,2,4 }));
+            Console.ReadLine();
+        }
+
+        public static int MaxProfit(int[] prices)
+        {
+            int len = prices.Length;
+
+            if (len < 2)
+            {
+                return 0;
+            }
+            
+            int[] sell = new int[len];
+            int[] buy = new int[len];
+
+            buy[0] = -prices[0];
+            buy[1] = -Math.Min(prices[0], prices[1]);
+            sell[1] = Math.Max(0, buy[0] + prices[1]);
+
+            for (int i = 2; i < len; i++)
+            {
+                buy[i] = Math.Max(buy[i - 1], sell[i - 2] - prices[i]);
+                sell[i] = Math.Max(sell[i - 1], buy[i - 1] + prices[i]);
+            }
+
+            return sell[len - 1];
+        }
+    }
+}
---
+++ LeetCodeDPDay8Program2/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace LeetCodeDPDay8Program2
+{
+    internal class BuySellStockTransactionFee
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MaxProfit(new int[] { 1, 3, 2, 8, 4, 9 }, 2));
+            Console.ReadLine();
+        }
+
+        public static int MaxProfit(int[] prices, int fee)
+        {
+            if (prices.Length < 2)
+            {
+                return 0;
+            }
+
+            var days = prices.Length;
+            var buy = new int[days];
+            var sell = new int[days];
+
+            buy[0] = -prices[0];
+
+            for (int i = 1; i < days; i++)
+            {
+                buy[i] = Math.Max(buy[i - 1], sell[i - 1] - prices[i]);
+                sell[i] = Math.Max(sell[i - 1], buy[i - 1] + prices[i] - fee);
+            }
+
+            return sell[days - 1];
+        }
+    }
+}
