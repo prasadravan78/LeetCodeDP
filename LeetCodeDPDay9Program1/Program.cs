71e50db548b8de400649422bf0a5401a9e23f522
Prasad Ravan <prasadcravan@gmail.com>
2022-08-10 23:09:10 +0530

    Day9

---
+++ LeetCodeDPDay9Program1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Runtime.Intrinsics.Arm;
+
+namespace LeetCodeDPDay9Program1
+{
+    internal class WordBreakProgram
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(WordBreak("applepenapple", new List<string>() { "apple", "pen" }));
+            Console.ReadLine();
+        }
+
+        public static bool WordBreak(string s, IList<string> wordDict)
+        {
+            var isValidDictionary = new bool[s.Length + 1];
+            isValidDictionary[0] = true;
+
+            for (int i = 1; i <= s.Length; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    if (isValidDictionary[j] && wordDict.Contains(s.Substring(j, i - j)))
+                    {
+                        isValidDictionary[i] = true;
+                        break;
+                    }
+                }
+            }
+
+            return isValidDictionary[s.Length];
+        }
+    }
+}
---
+++ LeetCodeDPDay9Program2/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace LeetCodeDPDay9Program2
+{
+    internal class TrappingRainWater
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Trap(new int[] { 0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1 }));
+            Console.ReadLine();
+        }
+
+        public static int Trap(int[] height)
+        {
+            var totalWater = 0;
+            var left = 0;
+            var right = height.Length - 1;
+            var maxLeft = 0;
+            var maxRight = 0;
+
+            if (height == null || height.Length < 1)
+            {
+                return totalWater;
+            }
+
+            while (left < right)
+            {
+                if (height[left] < height[right])
+                {
+                    if (height[left] >= maxLeft)
+                    {
+                        maxLeft = height[left];
+                    }
+                    else
+                    {
+                        totalWater += maxLeft - height[left];
+                    }
+                    left++;
+                }
+                else
+                {
+                    if (height[right] >= maxRight)
+                    {
+                        maxRight = height[right];
+                    }
+                    else
+                    {
+                        totalWater += maxRight - height[right];
+                    }
+                    right--;
+                }
+            }
+
+            return totalWater;
+        }
+    }
+}
